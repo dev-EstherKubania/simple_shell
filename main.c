044df8a5485f7dc9c80a8ff90c188fbe4e5901ae
dev-EstherKubania <eskury@gmail.com>
2023-07-18 03:18:01 -0700

    main function

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * main - open shell, project base
+ * Return: int
+ */
+int main(void)
+{
+        char *buff = NULL, **args;
+        size_t read_size = 0;
+        ssize_t buff_size = 0;
+        int exit_status = 0;
+
+        while (1)
+        {
+                if (isatty(0))
+                        printf("($) ");
+                buff_size = getline(&buff, &read_size, stdin);
+                if (buff_size == -1 || feof(stdin))
+        {
+                printf("\n");
+                break;
+        }
+                if (buff_size == 1 && buff[0] == '\n')
+                        continue;
+                buff[buff_size - 1] = '\0';
+                if (_strcmp("exit", buff) == 0)
+                        break;
+                if (_strcmp("env", buff) == 0)
+        {
+                _env();
+                        continue;
+        }
+                if (empty_line(buff) == 1)
+        {
+                exit_status = 0;
+                continue;
+        }
+                args = _split(buff, " ");
+                args[0] = search_path(args[0]);
+                if (args[0] != NULL)
+                exit_status = execute(args);
+                else
+                perror("Error");
+                free(args);
+        }
+                free(buff);
+        return (exit_status);
+}
