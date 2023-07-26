58db8fe1a437e5de8f1081d2e34c28b5cb8ac120
254guru <otienooluda@gmail.com>
2023-07-26 02:36:23 -0700

    execute function file

--- exec.c
+++ exec.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * execute - Execute a command
+ * execute - Execute a command with arguments
  *
  * @args: Array of strings containing command and arguments
  *
@@ -8,8 +8,74 @@
  */
 int execute(char **args)
 {
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
+	else if (_strcmp(args[0], "exit") == 0)
+	{
+		return (execute_exit(args));
+	}
+	else if (_strcmp(args[0], "env") == 0)
+	{
+		return (execute_env());
+	}
+	else
+	{
+		return (execute_command(args));
+	}
+}
+
+/**
+ * execute_exit - Handle the "exit" command
+ *
+ * @args: Array of strings containing command and arguments
+ *
+ * Return: Status code of the command execution
+ */
+int execute_exit(char **args)
+{
+	if (args[1] != NULL)
+	{
+		int status = _atoi(args[1]);
+
+		exit(status);
+	}
+	else
+	{
+		exit(EXIT_SUCCESS);
+	}
+}
+
+/**
+ * execute_env - Handle the "env" command
+ *
+ * Return: Status code of the command execution
+ */
+int execute_env(void)
+{
+	char **env = __environ;
+
+	while (*env != NULL)
+	{
+		_fputs(*env, stdout);
+		_fputc('\n', stdout);
+		env++;
+	}
+	return (1);
+}
+
+/**
+ * execute_command - Execute a command
+ *
+ * @args: Array of strings containing command and arguments
+ *
+ * Return: Status code of the command execution
+ */
+int execute_command(char **args)
+{
 	int status;
-	char *command_path = search_path(args[0]);
+	char *command_path = get_command_path(args[0]);
 
 	if (command_path != NULL)
 	{
@@ -17,16 +83,16 @@ int execute(char **args)
 
 		if (child_pid == 0)
 		{
-			if (execve(command_path, args, environ) == -1)
+			if (execve(command_path, args, NULL) == -1)
 			{
 				perror("execve");
-				exit(127);
+				exit(EXIT_FAILURE);
 			}
 		}
 		else if (child_pid < 0)
 		{
 			perror("fork");
-			exit(126);
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
@@ -36,5 +102,13 @@ int execute(char **args)
 		free(command_path);
 		}
 		}
-	return (status);
+		else
+		{
+		_fputs(program_name, stdout);
+		_fputs(": Command not found: ", stdout);
+		_fputs(args[0], stdout);
+		_fputc('\n', stdout);
+	}
+	return (0);
 }
+
